a783b08116cb3446a26f1008c0ac016fdbfc917e
tech75rus <tech75rus@mail.ru>
2022-08-13 17:00:17 +0900

    для front-end pwa сделал обновление прикаждом измениние файлов

---
+++ app/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pwa: {
+        name: 'gladkix.com',
+        themeColor: '#222',
+        workboxOptions: {
+            skipWaiting: true
+        }
+    },
+    devServer: {
+        proxy: 'https://127.0.0.1:8000/',
+    }
+}
