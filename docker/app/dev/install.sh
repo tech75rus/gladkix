793f4e57663f30cd857155dfc32ab62f47302d6b
tech75rus <tech75rus@mail.ru>
2022-08-31 20:56:34 +0900

    fix

--- docker/app/dev/install.sh
+++ docker/app/dev/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+mkdir src/service
+touch src/service/host.js
+echo "let localhost = 'https://localhost';
+
+export let host = localhost;" > /var/www/app/src/service/host.js
+
 yarn install
 
 yarn serve
