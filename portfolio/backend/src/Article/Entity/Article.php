d4274127a00fe73022ccab32cfdfcba8e30879f6
tech75rus <tech75rus@mail.ru>
2025-10-31 20:45:31 +0900

    created entity Tag and relation with Article

--- portfolio/backend/src/Article/Entity/Article.php
+++ portfolio/backend/src/Article/Entity/Article.php
@@ -4,6 +4,9 @@ namespace App\Article\Entity;
 
 use App\Article\Repository\ArticleRepository;
 use App\Category\Entity\Category;
+use App\Tag\Entity\Tag;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -104,6 +107,17 @@ class Article
 
     #[ORM\ManyToOne(inversedBy: 'Articles')]
     private ?Category $category = null;
+
+    /**
+     * @var Collection<int, Tag>
+     */
+    #[ORM\ManyToMany(targetEntity: Tag::class, mappedBy: 'articles')]
+    private Collection $tags;
+
+    public function __construct()
+    {
+        $this->tags = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -288,6 +302,33 @@ class Article
     public function setCategory(?Category $category): static
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): static
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags->add($tag);
+            $tag->addArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): static
+    {
+        if ($this->tags->removeElement($tag)) {
+            $tag->removeArticle($this);
+        }
 
         return $this;
     }
---
+++ portfolio/backend/src/Tag/Entity/Tag.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Tag\Entity;
+
+use App\Article\Entity\Article;
+use App\Tag\Repository\TagRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Tag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(
+        length: 50,
+        unique: true,
+        options: ['comment' => 'Название тега (например: PHP, JavaScript, DevOps и т.д.)']
+    )]
+    private ?string $name = null;
+
+    #[ORM\Column(
+        type: Types::TEXT, 
+        nullable: true,
+        options: ['comment' => 'Описание тега (необязательно)']
+    )]
+    private ?string $description = null;
+
+    #[ORM\Column(
+        options: ['comment' => 'Дата и время создания тега']
+    )]
+    private ?\DateTimeImmutable $createAt = null;
+
+    /**
+     * @var Collection<int, Article>
+     */
+    #[ORM\ManyToMany(targetEntity: Article::class, inversedBy: 'tags')]
+    private Collection $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->createAt;
+    }
+
+    #[ORM\PrePersist]
+    public function setCreateAt(\DateTimeImmutable $createAt): static
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Article>
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): static
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles->add($article);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): static
+    {
+        $this->articles->removeElement($article);
+
+        return $this;
+    }
+}
