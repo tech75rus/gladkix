20a8ac69405539b056d10127a0a01ce0821cc900
tech75rus <tech75rus@mail.ru>
2025-11-24 13:58:35 +0900

    menu active url color bg

--- portfolio/frontend/components/MenuAdmin.vue
+++ portfolio/frontend/components/MenuAdmin.vue
@@ -3,6 +3,7 @@
       <NuxtLink 
         to="/admin" 
         class="text-xl font-bold text-custom-11 flex items-center p-3 pr-20 rounded-lg hover:bg-custom-20 transition-colors duration-200"
+        :class="{ 'bg-custom-20': isActive('/admin') }"
       >
         <Icon name="ph:crown" class="mr-3 text-lg" />
         Admin
@@ -12,7 +13,8 @@
         <li v-for="item in menuItems" :key="item.to">
           <NuxtLink 
             :to="item.to" 
-            class="text-custom-11 flex items-center p-3 rounded-lg hover:bg-custom-20 transition-colors duration-200 "
+            class="flex items-center p-3 rounded-lg hover:bg-custom-600 transition-colors duration-200"
+            :class="{ 'bg-custom-50': isActive(item.to) }"
           >
             <Icon :name="item.icon" class="mr-3" />
             {{ item.label }}
@@ -20,7 +22,6 @@
         </li>
       </ul>
     </nav>
-
 </template>
 
 <script setup>
@@ -31,4 +32,15 @@ const menuItems = [
   { to: '/admin/tags', icon: 'ph:tag', label: 'Tags' },
   { to: '/admin/categories', icon: 'ph:folders', label: 'Categories' }
 ]
+
+const route = useRoute()
+
+// Функция проверки активного пути
+const isActive = (path) => {
+  return route.path.startsWith(path)
+}
 </script>
+
+<style scoped>
+
+</style>
