08eb11a8bb0344d8b189043718534fe6ef46f1fd
tech75rus <tech75rus@mail.ru>
2026-02-06 16:23:00 +0900

    Get data with server

--- portfolio/frontend/pages/admin/articles/index.vue
+++ portfolio/frontend/pages/admin/articles/index.vue
@@ -1,26 +1,17 @@
 <template>
-  <div class="flex p-8 border ml-4 h-[100%]">
-    <div 
-      class="bg-emerald-200 transition-all duration-300"
-      :class="size ? 'w-[600px]' : 'w-[300px]'"
-    >One</div>
-    <div class="bg-orange-200 w-full">Two</div>
-  </div>
-  <div class="fixed bottom-2 right-8">
-    <Button @click="changeSize">Click</Button>
-  </div>
+  <h2 class="text-3xl">Hello Articles</h2>
 </template>
 
-<script setup>
+<script setup lang="ts">
+const route = useRoute()
+
 definePageMeta({
   layout: 'admin'
 })
 
-const size = ref(false);
-
-const changeSize = () => {
-  size.value = !size.value;
-}
+const { data } = useFetch('http://localhost/api/articles', {
+  server: false
+});
 
 </script>
 
