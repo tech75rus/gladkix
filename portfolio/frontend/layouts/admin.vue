2108484f5fa28577e570a661aadf166951c1e9c3
tech75rus <tech75rus@mail.ru>
2026-02-17 18:18:28 +0900

    Добавил кнопку для полноэкранного режима для админки

--- portfolio/frontend/layouts/admin.vue
+++ portfolio/frontend/layouts/admin.vue
@@ -3,7 +3,10 @@
     <Toast />
     <MenuAdmin @close="open = $event" class="md:flex" />
     <MenuMobileAdmin class="md:hidden" />
-    <div class="flex-1 transition-all duration-300"
+    <Button raised severity="secondary" class="!fixed bottom-[17px] right-[15px] border z-50 bg-white" @click="toggleFullscreen">
+      <Icon :name="isFullscreen ? 'ph:arrows-in' : 'ph:arrows-out'" size="26" />
+    </Button>
+    <div class="flex-1 transition-all duration-300 max-md:mt-[50px] mb-[70px]"
       :class="[open ? 'md:ml-[235px]' : 'md:ml-[80px]']"
     >
       <slot></slot>
@@ -14,5 +17,29 @@
 <script setup>
 
 const open = ref(true);
+
+const isFullscreen = ref(false);
+
+// Функция переключения
+const toggleFullscreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+};
+
+// Слушаем изменение статуса
+const handleFullscreenChange = () => {
+  isFullscreen.value = !!document.fullscreenElement;
+};
+
+onMounted(() => {
+  document.addEventListener('fullscreenchange', handleFullscreenChange);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('fullscreenchange', handleFullscreenChange);
+});
 
 </script>
